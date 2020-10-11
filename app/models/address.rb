b7946132f5b559dc279672e65ded59082e7ac2ca
marcozsouto <marcozsouto@gmail.com>
2020-10-11 16:20:36 -0300

    updating models

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,6 +3,8 @@ class Address < ApplicationRecord
 	has_one :service, dependent: :destroy 
     
     validates :street, :neighborhood, :zipcode, :number, :city, presence:true
-  	validates_length_of :zipcode, maximum: 8
-  	
+	validates_length_of :zipcode, maximum: 8, minimum: 8
+	validates_numericality_of :number
+	validates_numericality_of :zipcode 
+	  
 end
